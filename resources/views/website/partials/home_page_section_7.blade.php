5464501a44355a11862a36ed624e0960f96bbaf2
HammamZarefa <HammamZarefa@gmail.com>
2022-03-14 13:10:20 +0200

    teacg=her section

--- resources/views/website/partials/home_page_section_7.blade.php
+++ resources/views/website/partials/home_page_section_7.blade.php
@@ -3,77 +3,28 @@
     <div class="container">
         <div class="row title">
             <div class="col-sm-12">
-                <h2>@lang('Meet the Teachers')</h2>
-                <p>@lang('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, distinctioptatem eligendi dolore numquam dolor quis ex velit esse').</p>
+                <h2>Meet the Teachers</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, distinctioptatem eligendi
+                    dolore numquam dolor quis ex velit esse.</p>
             </div>
         </div>
-
-
         <div class="row">
-
+            @foreach($writers as $writer)
             <div class="item col-sm-3 item">
                 <a href="#teacher01" data-toggle="modal">
                     <div class="class_title">
-                        <h2> @lang('Jason Smith') </h2>
+                        <h2> {{$writer->firstName.' '.$writer->lastName}} </h2>
                     </div>
                     <figure>
-                        <img src="{{ asset('front/img/teachers/teacher-01.jpg') }}" alt="">
-                        <figcaption><i class="icon-plus-circled"></i></figcaption>
+                        <img src="{{asset(Storage::url($writer->photo))}}" alt="">
+                        {{--<figcaption><i class="icon-plus-circled"></i></figcaption>--}}
                     </figure>
                     <div class="description">
-                        <h3><strong>@lang('Mathematics')</strong></h3>
-                        <p>@lang('Lorem ipsum dolor sit amet, consectetur adipisicing elit').</p>
+                        <p>{{$writer->bio}}</p>
                     </div>
                 </a>
             </div>
-
-            <div class="item col-sm-3 item">
-                <a href="#teacher02" data-toggle="modal">
-                    <div class="class_title">
-                        <h2> @lang('JAKE VALINER') </h2>
-                    </div>
-                    <figure>
-                        <img src="{{ asset('front/img/teachers/teacher-02.jpg') }}" alt="">
-                        <figcaption><i class="icon-plus-circled"></i></figcaption>
-                    </figure>
-                    <div class="description">
-                        <h3><strong>@lang('Marketing')</strong></h3>
-                        <p>@lang('Lorem ipsum dolor sit amet, consectetur adipisicing elit').</p>
-                    </div>
-                </a>
-            </div>
-
-            <div class="item col-sm-3 item">
-                <a href="#teacher03" data-toggle="modal">
-                    <div class="class_title">
-                        <h2> @lang('Tom Reedert') </h2>
-                    </div>
-                    <figure>
-                        <img src="{{ asset('front/img/teachers/teacher-03.jpg') }}" alt="">
-                        <figcaption><i class="icon-plus-circled"></i></figcaption>
-                    </figure>
-                    <div class="description">
-                        <h3><strong>@lang('web designer')</strong></h3>
-                        <p>@lang('Lorem ipsum dolor sit amet, consectetur adipisicing elit').</p>
-                    </div>
-                </a>
-            </div>
-
-            <div class="item col-sm-3 item">
-                <a href="#teacher04" data-toggle="modal">
-                    <div class="class_title">
-                        <h2> @lang('JAKE VALINER') </h2>
-                    </div>
-                    <figure>
-                        <img src="{{ asset('front/img/teachers/teacher-04.jpg') }}" alt="">
-                        <figcaption><i class="icon-plus-circled"></i></figcaption>
-                    </figure>
-                    <div class="description">
-                        <h3><strong>@lang('SEO')</strong></h3>
-                        <p>@lang('Lorem ipsum dolor sit amet, consectetur adipisicing elit').</p>
-                    </div>
-                </a>
-            </div>
+            @endforeach
         </div>
     </div>
 </section>
