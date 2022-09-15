c2c868dc2560e1060f3a9217cbad49d05d00309e
alaamhna3354 <76902248+alaamhna3354@users.noreply.github.com>
2022-09-15 17:23:34 -0400

    edit pdf in blog

--- resources/views/website/blog.blade.php
+++ resources/views/website/blog.blade.php
@@ -60,11 +60,13 @@
                                         <img src="{{ asset('storage/'.$post->cover) }}" alt="">
                                     @else
                                         {{--//Preview Pdf Here ----------------------------------------------}}
-                                        <object data="{{asset('images/blog/'.$post->body)}}" type="application/pdf"
-                                                width="750 px" height="700 px%">
+                                        <iframe 
+                                        src="http://docs.google.com/gview?url={{asset('images/blog/'.$post->body)}}&embedded=true"
+                                          type="application/pdf"
+                                                width="100%" height="100%">
                                             <p>Alternative text - include a link <a
                                                         href="{{asset('images/blog/'.$post->body)}}">to the PDF!</a></p>
-                                        </object>
+                                        </iframe >
                                         {{--// End of preview pdf--}}
                                     @endif
                                     <h2>{{$post->title}}</h2>
